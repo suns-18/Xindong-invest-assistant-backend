b40c3dae6c3b2b6b67f00be15bb2fe215a5d1a96
NBDatsuya <2646809050@qq.com>
2023-11-01 20:58:16 +0800

    feat: entity classes

---
+++ src/main/java/neu/xindong/ia/entity/QuestionTitle.java
@@ -0,0 +1,18 @@
+package neu.xindong.ia.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+// 加上@Data注解，自动帮你生成Getter和Setter
+// 加上@TableName注解，括号里写上表的名字
+// 加上@TableId注解，表示对应属性是数据库表的主码（键）
+// 数据库文档中的下划线表示单词间隔，类名单词首字母必须大写（大驼峰）
+// 变量名是小驼峰
+@Data
+@TableName("question_title")
+public class QuestionTitle {
+    @TableId
+    private Integer id;
+    private String title;
+}

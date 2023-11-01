731de1792545c4f65a140ed498f03c9630f28d94
NBDatsuya <2646809050@qq.com>
2023-11-01 20:58:52 +0800

    feat: dao

---
+++ src/main/java/neu/xindong/ia/dao/QuestionOptionDao.java
@@ -0,0 +1,10 @@
+package neu.xindong.ia.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import neu.xindong.ia.entity.QuestionOption;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionOptionDao extends BaseMapper<QuestionOption>{
+
+}

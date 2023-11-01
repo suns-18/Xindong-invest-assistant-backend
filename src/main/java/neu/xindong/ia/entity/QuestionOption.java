3cb563b456b8d5964f7d045fa190a603a97899b1
NBDatsuya <2646809050@qq.com>
2023-11-01 20:57:50 +0800

    feat: entity classes

---
+++ src/main/java/neu/xindong/ia/entity/QuestionOption.java
@@ -0,0 +1,16 @@
+package neu.xindong.ia.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("question_option")
+public class QuestionOption {
+    @TableId
+    private Integer id;
+    private Integer question;
+    private String title;
+    private Integer value;
+    private Integer questionType;
+}

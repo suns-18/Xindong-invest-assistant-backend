9bf57bb3fccbae7175fc5054f2f78b3cd3edfd29
NBDatsuya <2646809050@qq.com>
2023-11-01 20:57:45 +0800

    feat: entity class

---
+++ src/main/java/neu/xindong/ia/entity/Possession.java
@@ -0,0 +1,16 @@
+package neu.xindong.ia.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("possession")
+public class Possession {
+    @TableId
+    private Integer id;
+    private Integer productId;
+    private Double purchasePrice;
+    private Integer amount;
+    private Boolean sold;
+}

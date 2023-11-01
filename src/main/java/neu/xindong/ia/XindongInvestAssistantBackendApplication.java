09e697d2f654cdd81a24deb1d61dd587394a1963
NBDatsuya <2646809050@qq.com>
2023-11-01 21:07:24 +0800

    init commit

--- src/main/java/neu/xindong/ia/XindongInvestAssistantBackendApplication.java
+++ src/main/java/neu/xindong/ia/XindongInvestAssistantBackendApplication.java
@@ -1,9 +1,11 @@
 package neu.xindong.ia;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("neu.xindong.ia.dao")
 public class XindongInvestAssistantBackendApplication {
 
     public static void main(String[] args) {

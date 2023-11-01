ddaaeb6edb624ef17578fecfc49ab208312d290e
NBDatsuya <2646809050@qq.com>
2023-11-01 20:58:57 +0800

    feat: dao

---
+++ src/main/java/neu/xindong/ia/dao/AnswerDao.java
@@ -0,0 +1,10 @@
+package neu.xindong.ia.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import neu.xindong.ia.entity.Answer;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AnswerDao extends BaseMapper<Answer> {
+
+}
---
+++ src/main/java/neu/xindong/ia/dao/PossessionDao.java
@@ -0,0 +1,9 @@
+package neu.xindong.ia.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import neu.xindong.ia.entity.Possession;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PossessionDao extends BaseMapper<Possession> {
+}

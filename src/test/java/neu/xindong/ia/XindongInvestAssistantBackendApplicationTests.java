0cf89206d7024630bd1f81351647fa78976313a7
Sherlock <1064078316@qq.com>
2023-11-02 21:42:28 +0800

    feat: question test

--- src/test/java/neu/xindong/ia/XindongInvestAssistantBackendApplicationTests.java
+++ src/test/java/neu/xindong/ia/XindongInvestAssistantBackendApplicationTests.java
@@ -1,15 +1,23 @@
 package neu.xindong.ia;
 
+import neu.xindong.ia.controller.QuestionController;
+import org.apache.ibatis.jdbc.Null;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.relational.core.sql.In;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.logging.Logger;
 
 @SpringBootTest
 class XindongInvestAssistantBackendApplicationTests {
-
+    Logger log = Logger.getLogger(String.valueOf(XindongInvestAssistantBackendApplicationTests.class));
+    @Autowired
+    private QuestionController questionController;
     @Test
     void sortEasier(){
         var array = new ArrayList<Integer>();
@@ -21,5 +29,13 @@ class XindongInvestAssistantBackendApplicationTests {
 
         array.sort(Comparator.reverseOrder());
         System.out.println(array);
+    }
+    /*对questionController的测试*/
+    @Test
+    void Question(){
+        Assertions.assertEquals(questionController.queryList(), null
+                , "Project模块>>列表请求测试1：返回所有，未通过");
+        log.info("Project模块>>列表请求测试1：返回所有，通过");
+        log.info("Question模块>>列表请求测试通过");
     }
 }

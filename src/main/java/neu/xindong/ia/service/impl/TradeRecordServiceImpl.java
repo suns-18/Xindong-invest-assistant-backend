8b55c29703704d12b2ea6f511e8df95c3e9d57ae
AaaNa <2915118445@qq.com>
2023-11-02 20:49:11 +0800

    feat: QuestionOptionServiceImpl,QuestionTitleServiceImpl,TradeRecordServiceImpl

--- src/main/java/neu/xindong/ia/service/impl/TradeRecordServiceImpl.java
+++ src/main/java/neu/xindong/ia/service/impl/TradeRecordServiceImpl.java
@@ -2,13 +2,14 @@ package neu.xindong.ia.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import neu.xindong.ia.dao.TradeRecordDao;
+import neu.xindong.ia.entity.Product;
 import neu.xindong.ia.entity.TradeRecord;
 import neu.xindong.ia.service.TradeRecordService;
 import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class TradeRecordServiceImpl extends ServiceImpl<TradeRecordDao, TradeRecord> implements TradeRecordService {
-    /**
+    /**code by ryr
      * trade list
      * @return
      */
@@ -17,5 +18,15 @@ public class TradeRecordServiceImpl extends ServiceImpl<TradeRecordDao, TradeRec
                 .eq("tradeRecord", tradeRecord)
                 .list();
     }
+
+    /**code by ryr
+     * get tradeRecord by product id
+     * @param product
+     * @return
+     */
+    public List<TradeRecord> findTradeRecordByProductId(Product product){
+        return query().like("product_id",product.getId()).list();
+    }
+
 
 }

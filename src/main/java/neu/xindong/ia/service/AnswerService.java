c2b8dc79ea96d30204559f21bbec80076ce62b37
NBDatsuya <2646809050@qq.com>
2023-11-02 21:23:03 +0800

    fix: service method prototype

--- src/main/java/neu/xindong/ia/service/AnswerService.java
+++ src/main/java/neu/xindong/ia/service/AnswerService.java
@@ -2,5 +2,13 @@ package neu.xindong.ia.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import neu.xindong.ia.entity.Answer;
+
+import java.util.List;
+
 public interface AnswerService extends IService<Answer> {
+    List<Answer> findAll();
+
+    Answer findAntiRiskQuestionFromAnswer();
+    Answer findStabilityQuestionFromAnswer();
+    Answer findReturnRateQuestionFromAnswer();
 }
--- src/main/java/neu/xindong/ia/service/PossessionService.java
+++ src/main/java/neu/xindong/ia/service/PossessionService.java
@@ -3,5 +3,8 @@ package neu.xindong.ia.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import neu.xindong.ia.entity.Possession;
 
+import java.util.List;
+
 public interface PossessionService extends IService<Possession> {
+    List<Possession> findAll();
 }

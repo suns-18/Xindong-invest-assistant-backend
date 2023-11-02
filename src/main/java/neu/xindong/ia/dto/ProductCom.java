aa0581a9a085375c6c523e7be8b97731f52983af
AaaNa <2915118445@qq.com>
2023-11-02 20:29:51 +0800

    feat: ProductCom

---
+++ src/main/java/neu/xindong/ia/dto/ProductCom.java
@@ -0,0 +1,9 @@
+package neu.xindong.ia.dto;
+
+import lombok.Data;
+import neu.xindong.ia.entity.Product;
+@Data
+public class ProductCom {
+    private Product product;
+    private Double comprehensive;
+}

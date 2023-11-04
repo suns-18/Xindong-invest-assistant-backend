f06f81ef4bbdbc81c8c0c6db451043342e632738
AaaNa <2915118445@qq.com>
2023-11-04 17:04:41 +0800

    feat:TradeRecordService,TradeRecordController

--- src/main/java/neu/xindong/ia/controller/TradeRecordController.java
+++ src/main/java/neu/xindong/ia/controller/TradeRecordController.java
@@ -1,9 +1,41 @@
 package neu.xindong.ia.controller;
 
+import neu.xindong.ia.dto.HttpResponse;
+import neu.xindong.ia.entity.Product;
+import neu.xindong.ia.entity.TradeRecord;
+import neu.xindong.ia.service.TradeRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+/**code by ryr
+ * tradeRecordController
+ */
 @RestController
 @RequestMapping("/api/tradeRecord")
 public class TradeRecordController {
+    @Autowired
+    private TradeRecordService tradeRecordService;
+    @GetMapping(value = "/queryList")
+    public HttpResponse getAllTradeRecord() {
+        List<TradeRecord> tradeRecordList=tradeRecordService.findAll();
+        return HttpResponse.builder()
+                .code(200)
+                .message("Get trade record list successfully")
+                .data(tradeRecordList)
+                .build();
+    }
+    @GetMapping(value = "/queryByProductId")
+    public HttpResponse getTradeRecordByProductId(@RequestBody Product product){
+        List<TradeRecord> tradeRecordList=tradeRecordService.findTradeRecordByProductId(product);
+        return HttpResponse.builder()
+                .code(200)
+                .message("Get trade record list by productId successfully")
+                .data(tradeRecordList)
+                .build();
+    }
 }
--- src/main/java/neu/xindong/ia/service/TradeRecordService.java
+++ src/main/java/neu/xindong/ia/service/TradeRecordService.java
@@ -1,8 +1,16 @@
 package neu.xindong.ia.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import neu.xindong.ia.entity.Product;
 import neu.xindong.ia.entity.TradeRecord;
 
+import java.util.List;
+
+/**code by ryr
+ * abstract method
+ */
 public interface TradeRecordService extends IService<TradeRecord> {
+    List<TradeRecord> findAll();
+    List<TradeRecord> findTradeRecordByProductId(Product product);
 
 }

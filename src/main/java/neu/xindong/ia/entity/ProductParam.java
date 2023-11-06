1482941199bde41a87f836f5bbfe85db113ada48
wangwenqingqq <1064078316@qq.com>
2023-11-06 20:08:58 +0800

    feat: ProductParam

--- src/main/java/neu/xindong/ia/entity/ProductParam.java
+++ src/main/java/neu/xindong/ia/entity/ProductParam.java
@@ -1,8 +1,12 @@
 package neu.xindong.ia.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@TableName("product_param")
+@ToString
 public class ProductParam {
     private Integer id;
     private Integer productId;

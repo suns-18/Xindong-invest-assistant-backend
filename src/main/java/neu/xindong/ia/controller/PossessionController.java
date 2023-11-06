9cd5d658f9a674ae81794012a0a3e5d5d264f08f
wangwenqingqq <1064078316@qq.com>
2023-11-06 17:10:36 +0800

    fix: PossessionController

--- src/main/java/neu/xindong/ia/controller/PossessionController.java
+++ src/main/java/neu/xindong/ia/controller/PossessionController.java
@@ -1,9 +1,10 @@
 package neu.xindong.ia.controller;
 
 import neu.xindong.ia.dto.HttpResponse;
+import neu.xindong.ia.dto.PossessionItem;
 import neu.xindong.ia.entity.Possession;
 import neu.xindong.ia.entity.TradeRecord;
-import neu.xindong.ia.service.PossessionitemService;
+import neu.xindong.ia.service.PossessionItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,82 +17,98 @@ import java.util.List;
 @RequestMapping("/api/possession")
 public class PossessionController {
     @Autowired
-    private PossessionitemService possessionitemService;
+    private PossessionItemService possessionItemService;
     @GetMapping("/all")
     public HttpResponse getAllPossessions() {
-        List<Possession> possessions = possessionitemService.findAll();
+        try {
+        List<PossessionItem> possessions = possessionItemService.findAll();
+        return HttpResponse.builder()
+                .code(200)
+                .data(possessions)
+                .message("成功获取所有持仓信息")
+                .build();
+        } catch (Exception e) {
+            return HttpResponse.builder()
+                    .code(0)
+                    .message("数据库访问错误")
+                    .build();
+        }
+    }
+    @GetMapping("/totalAssets")
+    public HttpResponse getTotalAssets(){
+
         return HttpResponse.builder()
                 .code(200)
                 .message("成功获取所有持仓信息")
                 .build();
     }
 
-    @GetMapping("/add")
-    public HttpResponse addPossession(@RequestBody TradeRecord tradeRecord){
-        try {
-            boolean result = possessionitemService.addPossession(tradeRecord);
-            if (result){
-                return HttpResponse.builder()
-                        .code(200)
-                        .message("成功添加持仓记录")
-                        .build();
-            }else {
-                return HttpResponse.builder()
-                        .code(0)
-                        .message("添加持仓记录失败")
-                        .build();
-            }
-        } catch (Exception e) {
-            return HttpResponse.builder()
-                    .code(0)
-                    .message("数据库访问错误")
-                    .build();
-        }
-    }
-
-    @GetMapping("/sell")
-    public HttpResponse updatePossessionWhenSell(@RequestBody TradeRecord tradeRecord){
-        try {
-            boolean result = possessionitemService.updatePossessionWhenSell(tradeRecord);
-            if (result){
-                return HttpResponse.builder()
-                        .code(200)
-                        .message("成功更新卖出持仓记录")
-                        .build();
-            }else {
-                return HttpResponse.builder()
-                        .code(0)
-                        .message("添加持仓记录失败")
-                        .build();
-            }
-        } catch (Exception e) {
-            return HttpResponse.builder()
-                    .code(0)
-                    .message("数据库访问错误")
-                    .build();
-        }
-    }
-
-    @GetMapping("/buy")
-    public HttpResponse updatePossessionWhenBuy(@RequestBody TradeRecord tradeRecord){
-        try {
-            boolean result = possessionitemService.updatePossessionWhenBuy(tradeRecord);
-            if (result){
-                return HttpResponse.builder()
-                        .code(200)
-                        .message("成功更新买入持仓记录")
-                        .build();
-            }else {
-                return HttpResponse.builder()
-                        .code(0)
-                        .message("更新买入持仓记录失败")
-                        .build();
-            }
-        } catch (Exception e) {
-            return HttpResponse.builder()
-                    .code(0)
-                    .message("数据库访问错误")
-                    .build();
-        }
-    }
+//    @GetMapping("/add")
+//    public HttpResponse addPossession(@RequestBody TradeRecord tradeRecord){
+//        try {
+//            boolean result = possessionItemService.addPossession(tradeRecord);
+//            if (result){
+//                return HttpResponse.builder()
+//                        .code(200)
+//                        .message("成功添加持仓记录")
+//                        .build();
+//            }else {
+//                return HttpResponse.builder()
+//                        .code(0)
+//                        .message("添加持仓记录失败")
+//                        .build();
+//            }
+//        } catch (Exception e) {
+//            return HttpResponse.builder()
+//                    .code(0)
+//                    .message("数据库访问错误")
+//                    .build();
+//        }
+//    }
+//
+//    @GetMapping("/sell")
+//    public HttpResponse updatePossessionWhenSell(@RequestBody TradeRecord tradeRecord){
+//        try {
+//            boolean result = possessionitemService.updatePossessionWhenSell(tradeRecord);
+//            if (result){
+//                return HttpResponse.builder()
+//                        .code(200)
+//                        .message("成功更新卖出持仓记录")
+//                        .build();
+//            }else {
+//                return HttpResponse.builder()
+//                        .code(0)
+//                        .message("添加持仓记录失败")
+//                        .build();
+//            }
+//        } catch (Exception e) {
+//            return HttpResponse.builder()
+//                    .code(0)
+//                    .message("数据库访问错误")
+//                    .build();
+//        }
+//    }
+//
+//    @GetMapping("/buy")
+//    public HttpResponse updatePossessionWhenBuy(@RequestBody TradeRecord tradeRecord){
+//        try {
+//            boolean result = possessionitemService.updatePossessionWhenBuy(tradeRecord);
+//            if (result){
+//                return HttpResponse.builder()
+//                        .code(200)
+//                        .message("成功更新买入持仓记录")
+//                        .build();
+//            }else {
+//                return HttpResponse.builder()
+//                        .code(0)
+//                        .message("更新买入持仓记录失败")
+//                        .build();
+//            }
+//        } catch (Exception e) {
+//            return HttpResponse.builder()
+//                    .code(0)
+//                    .message("数据库访问错误")
+//                    .build();
+//        }
+//    }
 }

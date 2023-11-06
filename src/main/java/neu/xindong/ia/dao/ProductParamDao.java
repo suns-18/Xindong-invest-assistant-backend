a07cd2ee4f4e1ab1c08fbf9577c6b8a9f4970ffe
wangwenqingqq <1064078316@qq.com>
2023-11-06 20:12:36 +0800

    feat: ProductParamDao

---
+++ src/main/java/neu/xindong/ia/dao/ProductParamDao.java
@@ -0,0 +1,11 @@
+package neu.xindong.ia.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import neu.xindong.ia.entity.Product;
+import neu.xindong.ia.entity.ProductParam;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProductParamDao extends BaseMapper<ProductParam> {
+
+}

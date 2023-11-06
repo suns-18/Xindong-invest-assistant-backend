1c24650aebb42a5f56e1e0577373e156ef29bd27
NBDatsuya <2646809050@qq.com>
2023-11-07 02:20:19 +0800

    refactor: rename

--- src/main/java/neu/xindong/ia/service/QuestionTitleService.java
+++ src/main/java/neu/xindong/ia/service/QuestionTitleService.java
@@ -6,5 +6,7 @@ import neu.xindong.ia.entity.QuestionTitle;
 import java.util.List;
 
 public interface QuestionTitleService extends IService<QuestionTitle> {
-    List<QuestionTitle> findQuestionTitleByQuestion(Integer question);
+    QuestionTitle findTitleById(Integer id);
+
+    List<QuestionTitle> findAll();
 }

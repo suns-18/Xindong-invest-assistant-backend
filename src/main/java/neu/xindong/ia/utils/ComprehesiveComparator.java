e9553216de039b3c8043fb28d6198a8d03df6904
AaaNa <2915118445@qq.com>
2023-11-06 11:12:59 +0800

    feat:comprehensiveComparator(change p1 and p2,naturalSort)

--- src/main/java/neu/xindong/ia/utils/ComprehesiveComparator.java
+++ src/main/java/neu/xindong/ia/utils/ComprehesiveComparator.java
@@ -2,14 +2,13 @@ package neu.xindong.ia.utils;
 
 import neu.xindong.ia.dto.ProductCom;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 
 public class ComprehesiveComparator implements Comparator<ProductCom> {
 
     @Override
     public int compare(ProductCom p1, ProductCom p2) {
-        return Double.compare(p2.getComprehensive(),
-                p1.getComprehensive());
+        return Double.compare(p1.getComprehensive(),
+                p2.getComprehensive());
     }
 }

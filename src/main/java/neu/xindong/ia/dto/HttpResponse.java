62ccaea6483b16fa511ed3d4cb1d8659b431c327
NBDatsuya <2646809050@qq.com>
2023-11-07 02:19:41 +0800

    refactor: HttpResponse

--- src/main/java/neu/xindong/ia/dto/HttpResponse.java
+++ src/main/java/neu/xindong/ia/dto/HttpResponse.java
@@ -5,8 +5,29 @@ import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
 @Data
+/*
 public class HttpResponse {
     private int code;
     private Object data;
     private String message;
+}*/
+
+public record HttpResponse<T>(
+        int code, T data, String msg) {
+    public static <T> HttpResponse<T> success(T data) {
+        return new HttpResponse<T>(
+                200, data, "请求成功"
+        );
+    }
+
+    public static <T> HttpResponse<T> success() {
+        return success(null);
+    }
+
+    public static <T> HttpResponse<T> failure(
+            int code, String msg) {
+        return new HttpResponse<T>(
+                code, null, msg
+        );
+    }
 }

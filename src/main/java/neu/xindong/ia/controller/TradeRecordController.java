e08118c3e6fc741e77a5da4e08e411858546e5c5
NBDatsuya <2646809050@qq.com>
2023-11-07 23:13:40 +0800

    BIG PROBLEM: the ambiguous attribute name "price" and "cost"

--- src/main/java/neu/xindong/ia/controller/TradeRecordController.java
+++ src/main/java/neu/xindong/ia/controller/TradeRecordController.java
@@ -7,10 +7,7 @@ import neu.xindong.ia.entity.Product;
 import neu.xindong.ia.entity.TradeRecord;
 import neu.xindong.ia.service.TradeRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,7 +34,8 @@ public class TradeRecordController {
     @Operation(summary = "查询交易记录",
             description = "通过产品编号查询，返回交易记录列表")
     public HttpResponse<List<TradeRecord>>
-    getTradeRecordByProductId(@RequestBody Product product) {
+    getTradeRecordByProductId(
+            @RequestBody Product product) {
         try {
             List<TradeRecord> tradeRecordList =
                     tradeRecordService
@@ -46,6 +44,31 @@ public class TradeRecordController {
         } catch (Exception e) {
             return HttpResponse.failure(0, "数据库访问错误");
         }
+    }
 
+    @PostMapping("/purchase")
+    @Operation(summary = "产品买入",
+            description = "买入对应产品ID的产品，传入的数据带有，返回操作结果")
+    public HttpResponse<Object> purchase(
+            @RequestBody TradeRecord record) {
+        try {
+            tradeRecordService.purchase(record);
+            return HttpResponse.success();
+        } catch (Exception e) {
+            return HttpResponse.failureWhenAccessDB();
+        }
+    }
+
+    @PostMapping("/sell")
+    @Operation(summary = "产品卖出",
+            description = "卖出对应交易记录ID的产品，返回操作结果")
+    public HttpResponse<Object> sell(
+            @RequestBody TradeRecord record) {
+        try {
+            tradeRecordService.sell(record);
+            return HttpResponse.success();
+        } catch (Exception e) {
+            return HttpResponse.failureWhenAccessDB();
+        }
     }
 }

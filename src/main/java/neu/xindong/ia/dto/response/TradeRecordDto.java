d91164b330cc76027fbe54cba9922fb2b0848952
NBDatsuya <2646809050@qq.com>
2023-11-11 17:06:03 +0800

    refactor: TradeRecordDto

--- src/main/java/neu/xindong/ia/dto/response/TradeRecordDto.java
+++ src/main/java/neu/xindong/ia/dto/response/TradeRecordDto.java
@@ -1,11 +1,14 @@
 package neu.xindong.ia.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.experimental.SuperBuilder;
 import neu.xindong.ia.entity.Product;
 
 import java.util.Date;
 
 @SuperBuilder
+@Data
 public class TradeRecordDto {
     Integer id;
     Product product;
